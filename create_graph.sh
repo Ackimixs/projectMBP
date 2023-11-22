81f47f60ad781463b50b06cc1a64e3eacf326959
Ackimixs_ <ackimixs@protonmail.com>
2023-11-22 22:36:37 +0100

    omg

--- create_graph.sh
+++ create_graph.sh
@@ -6,7 +6,7 @@ mkdir -p ./"$1"/graph_gen
 
 for PROB in .25 .5 .75
 do
-  for SIZE in 10 28 # 100 1000 3000 10000
+  for SIZE in 10 28 100 1000 3000 10000
   do
     $EXECUTABLE generate -n $SIZE -p $PROB -o ./"$1"/graph_gen/random_${SIZE}_${PROB}.in --debug
 
