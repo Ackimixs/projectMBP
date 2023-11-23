a6a9acdc13f42d0e2113b30a08668568f57c466c
Ackimixs_ <ackimixs@protonmail.com>
2023-11-23 16:29:53 +0100

    omg

--- create_graph.sh
+++ create_graph.sh
@@ -9,13 +9,5 @@ do
   for SIZE in 10 28 100 1000 3000 10000
   do
     $EXECUTABLE generate -n $SIZE -p $PROB -o ./"$1"/graph_gen/random_${SIZE}_${PROB}.in --debug
-
-    for OUT_DIR in exact constructive local_search tabu_search new_instances
-    do
-      if [ $OUT_DIR = exact ] && [ $SIZE -gt 20 ]; then
-        continue
-      fi
-      cp ./"$1"/graph_gen/random_${SIZE}_${PROB}.in ./instances/$OUT_DIR/random_${SIZE}_${PROB}.in
-    done
   done
 done
