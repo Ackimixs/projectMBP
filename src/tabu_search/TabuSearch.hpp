c2603b6dd2c97ed56ca50bc1ef6e92f09f68d16a
Ackimixs <Ackimixs@protonmail.com>
2023-12-05 08:57:54 +0100

    Update TabuSearch.hpp

--- src/tabu_search/TabuSearch.hpp
+++ src/tabu_search/TabuSearch.hpp
@@ -24,4 +24,4 @@ namespace TabuSearch_Utils {
     void LocalSearch(std::pair<std::vector<int>, std::vector<int>> &partition, const Graph &g, int &cutSize);
 }
 
-namespace TabuSearch = TabuSearch_V2;
+namespace TabuSearch = TabuSearch_V1;
