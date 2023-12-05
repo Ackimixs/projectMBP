a1a9308d710cbfe01a4c60b72fce91da4b18bf15
Ackimixs <Ackimixs@protonmail.com>
2023-12-05 08:26:54 +0100

    Update create_graph.sh

--- create_graph.sh
+++ create_graph.sh
@@ -4,10 +4,10 @@ EXECUTABLE=./"$1"/projectMBP
 
 mkdir -p ./"$1"/graph_gen
 
-for PROB in .25 .5 .75
+for PROB in 25 50 75
 do
   for SIZE in 10 28 100 1000 3000 10000
   do
-    $EXECUTABLE generate -n $SIZE -p $PROB -o ./"$1"/graph_gen/random_${SIZE}_${PROB}.in --debug
+    $EXECUTABLE generate -n $SIZE -p .$PROB -o ./"$1"/graph_gen/random_${SIZE}_${PROB}.in --debug
   done
 done
